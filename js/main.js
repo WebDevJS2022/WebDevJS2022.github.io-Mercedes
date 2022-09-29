d0c155dfb274fb08f36602dbf09b2668f6b3f02d
WebDevJS2022 <111743781+WebDevJS2022@users.noreply.github.com>
2022-09-29 11:52:59 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.design-slider').slick({
+        dots: false,
+        slidersToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="">',
+        nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="">',
+    });
+});
